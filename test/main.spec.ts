e3f521e9670e3842bf501b5ec230b54a9ed01b37
Arthur D'Andréa <aalemmar@gmail.com>
2020-10-21 17:02:20 -0300

    test: use snapshot matches on main.spec

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,104 +1,45 @@
-import translations from '../src/data/feedback/en'
 import zxcvbn from '../src/main'
-import passwordTests from './helper/passwordTests'
 
 describe('main', () => {
   it('should check without userInputs', () => {
-    const { calcTime, ...result } = zxcvbn('test')
-    expect(calcTime).toBeDefined()
-    expect(result).toEqual({
-      crackTimesDisplay: {
-        offlineFastHashing1e10PerSecond: 'less than a second',
-        offlineSlowHashing1e4PerSecond: 'less than a second',
-        onlineThrottling10PerSecond: '9 seconds',
-        onlineThrottling100PerHour: '56 minutes',
-      },
-      crackTimesSeconds: {
-        offlineFastHashing1e10PerSecond: 9.4e-9,
-        offlineSlowHashing1e4PerSecond: 0.0094,
-        onlineThrottling10PerSecond: 9.4,
-        onlineThrottling100PerHour: 3384,
-      },
-      feedback: {
-        suggestions: [translations.suggestions.anotherWord],
-        warning: translations.warnings.topHundred,
-      },
-      guesses: 94,
-      guessesLog10: 1.9731278535996983,
-      password: 'test',
-      score: 0,
-      sequence: [
-        {
-          baseGuesses: 93,
-          dictionaryName: 'passwords',
-          guesses: 93,
-          guessesLog10: 1.968482948553935,
-          i: 0,
-          j: 3,
-          l33t: false,
-          l33tVariations: 1,
-          matchedWord: 'test',
-          pattern: 'dictionary',
-          rank: 93,
-          reversed: false,
-          token: 'test',
-          uppercaseVariations: 1,
-        },
-      ],
+    const result = zxcvbn('test')
+    expect(result).toMatchSnapshot({
+      calcTime: expect.any(Number),
     })
   })
 
   it('should check with userInputs', () => {
-    const { calcTime, ...result } = zxcvbn('test', ['test', 12, true, []])
-    expect(calcTime).toBeDefined()
-    expect(result).toEqual({
-      crackTimesDisplay: {
-        offlineFastHashing1e10PerSecond: 'less than a second',
-        offlineSlowHashing1e4PerSecond: 'less than a second',
-        onlineThrottling10PerSecond: 'less than a second',
-        onlineThrottling100PerHour: '1 minute',
-      },
-      crackTimesSeconds: {
-        offlineFastHashing1e10PerSecond: 2e-10,
-        offlineSlowHashing1e4PerSecond: 0.0002,
-        onlineThrottling10PerSecond: 0.2,
-        onlineThrottling100PerHour: 72,
-      },
-      feedback: {
-        suggestions: [translations.suggestions.anotherWord],
-        warning: '',
-      },
-      guesses: 2,
-      guessesLog10: 0.30102999566398114,
-      password: 'test',
-      score: 0,
-      sequence: [
-        {
-          baseGuesses: 1,
-          dictionaryName: 'userInputs',
-          guesses: 1,
-          guessesLog10: 0,
-          i: 0,
-          j: 3,
-          l33t: false,
-          l33tVariations: 1,
-          matchedWord: 'test',
-          pattern: 'dictionary',
-          rank: 1,
-          reversed: false,
-          token: 'test',
-          uppercaseVariations: 1,
-        },
-      ],
+    const result = zxcvbn('test', ['test', 12, true, []])
+    expect(result).toMatchSnapshot({
+      calcTime: expect.any(Number),
     })
   })
 
   describe('password tests', () => {
-    passwordTests.forEach((data) => {
-      it(`should resolve ${data.password}`, () => {
-        const { calcTime, ...result } = zxcvbn(data.password)
-        expect(calcTime).toBeDefined()
-        expect(JSON.stringify(result)).toEqual(JSON.stringify(data.result))
+    ;[
+      '1q2w3e4r5t',
+      '1Q2w3e4r5t',
+      '1q2w3e4r5T',
+      'abcdefg123',
+      'TESTERINO',
+      'aaaaaaa',
+      'Daniel',
+      '1234qwer',
+      '1234qwe',
+      '1234qwert',
+      'password',
+      '2010abc',
+      'abcabcabcabc',
+      'qwer',
+      'P4$$w0rd',
+      'aA!1',
+      'dgo9dsghasdoghi8/!&IT/§(ihsdhf8o7o',
+    ].forEach((password) => {
+      it(`should resolve ${password}`, () => {
+        const result = zxcvbn(password)
+        expect(result).toMatchSnapshot({
+          calcTime: expect.any(Number),
+        })
       })
     })
   })
